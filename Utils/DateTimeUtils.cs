907af3c34c06cde459f1cb565c48bbbf7af654c8
Guillaume Teirlinck <guillaume.teirlinck@gmail.com>
2014-12-18 07:50:23 +0000

    git-svn-id: svn://svn.teirlinck.net/Net/Utils@192 284496cf-f678-421e-8c4e-4a4e0f0d378f

--- Utils/DateTimeUtils.cs
+++ Utils/DateTimeUtils.cs
@@ -8,5 +8,11 @@ namespace Net.Teirlinck.Utils
 {
     public static class DateTimeUtils
     {
+        public static DateTime GetFromUnixTimeStamp(long unixTimeStamp)
+        {
+            DateTime dt = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+
+            return dt.AddSeconds(unixTimeStamp).ToLocalTime();
+        }
     }
 }
