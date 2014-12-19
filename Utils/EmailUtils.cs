4a40228c2ad47db7af47c989ae860cc8894a4ddf
Guillaume Teirlinck <guillaume.teirlinck@gmail.com>
2014-12-19 03:43:49 +0000

    added EmailUtils
    git-svn-id: svn://svn.teirlinck.net/Net/Utils@221 284496cf-f678-421e-8c4e-4a4e0f0d378f

---
+++ Utils/EmailUtils.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net.Teirlinck.Utils
+{
+    public static class EmailUtils
+    {
+        private static string smtpAddress = "smtp.gmail.com";
+        private static int portNumber = 587;
+        private static bool enableSSL = true;
+
+        public static void SendEmailFromFXTrader(string subject, string body, IEnumerable<string> attachments = null)
+        {
+            string emailFrom = "fxtrader@teirlinck.net";
+            string password = "FAKanYRSYY";
+            string emailTo = "fxtrader@teirlinck.net";
+
+            using (MailMessage mail = new MailMessage())
+            {
+                mail.From = new MailAddress(emailFrom);
+                mail.To.Add(emailTo);
+                mail.Subject = subject;
+                mail.Body = body;
+                mail.IsBodyHtml = true;
+
+                if (attachments != null)
+                {
+                    foreach (string attachment in attachments)
+                        mail.Attachments.Add(new Attachment(attachment));
+                }
+
+                using (SmtpClient smtpClient = new SmtpClient(smtpAddress, portNumber))
+                {
+                    smtpClient.Credentials = new NetworkCredential(emailFrom, password);
+                    smtpClient.EnableSsl = enableSSL;
+                    smtpClient.Send(mail);
+                }
+            }
+        }
+    }
+}
