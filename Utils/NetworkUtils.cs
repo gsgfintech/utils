517c1c9515f4b9ab7478e5fba14775bcf70b5ab8
Guillaume Teirlinck <guillaume.teirlinck@gmail.com>
2014-12-20 13:44:45 +0000

    moved logic to new utils class
    git-svn-id: svn://svn.teirlinck.net/Net/Utils@245 284496cf-f678-421e-8c4e-4a4e0f0d378f

---
+++ Utils/NetworkUtils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Net.Teirlinck.Utils
+{
+    public static class NetworkUtils
+    {
+        public static bool IsProcessListening(string host, int port)
+        {
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                try
+                {
+                    socket.Connect(host, port);
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
