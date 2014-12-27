a6e5fa23c797594a6134d69deb6c48076a2475c7
Guillaume Teirlinck <guillaume.teirlinck@gmail.com>
2014-12-27 01:30:46 +0000

    added IsLargerThan<T>
    git-svn-id: svn://svn.teirlinck.net/Net/Utils@260 284496cf-f678-421e-8c4e-4a4e0f0d378f

--- Utils/CollectionUtils.cs
+++ Utils/CollectionUtils.cs
@@ -12,5 +12,17 @@ namespace Net.Teirlinck.Utils
         {
             return ((enumerable == null) || (enumerable.Count<T>() == 0));
         }
+
+        /// <summary>
+        /// Returns true if an enumerable is not null and has a size larger than a certain minimum
+        /// </summary>
+        /// <typeparam name="T">The type of the generic enumerable</typeparam>
+        /// <param name="enumerable">The enumerable to test</param>
+        /// <param name="largerThan">The minimum sixze (inclusive)</param>
+        /// <returns></returns>
+        public static bool IsLargerThan<T>(IEnumerable<T> enumerable, int largerThan)
+        {
+            return ((enumerable != null) && (enumerable.Count<T>() >= largerThan));
+        }
     }
 }
