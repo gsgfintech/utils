b0050ef7e2c75d33c1218bd094b303a363a224e2
Guillaume Teirlinck <guillaume.teirlinck@gmail.com>
2015-07-24 10:18:01 +0800

    Make NetworkUtils.IsProcessListening stronger by ensuring that any successfull socket connection is closed properly

---
+++ UtilsTests/NetworkUtilsTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Net.Teirlinck.Utils;
+
+namespace UtilsTests
+{
+    [TestClass]
+    public class NetworkUtilsTests
+    {
+        [TestMethod]
+        public void TestIsProcessListening()
+        {
+            // 1. Test something that exists (FXMonitor)
+            string host = "gsg-prod-1.gsg.capital";
+            int port = 11000;
+
+            bool socketStatus = NetworkUtils.IsProcessListening(host, port);
+
+            Assert.IsTrue(socketStatus);
+
+            // 2. Test something that does not exist
+            host = "gsg-prod-1.gsg.capital";
+            port = 12000;
+
+            socketStatus = NetworkUtils.IsProcessListening(host, port);
+
+            Assert.IsFalse(socketStatus);
+        }
+    }
+}
