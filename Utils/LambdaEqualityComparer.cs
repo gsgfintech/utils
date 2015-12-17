09178769a6bfc40a71b5fcc6f53d4527a3c83a74
Guillaume Teirlinck <guillaume.teirlinck@gmail.com>
2015-12-17 10:30:51 +0800

    Add class LambdaEqualityComparer

---
+++ Utils/LambdaEqualityComparer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Net.Teirlinck.Utils
+{
+    public class LambdaEqualityComparer<T> : IEqualityComparer<T>
+    {
+        private Func<T, T, bool> _equalsFunction;
+        private Func<T, int> _hashCodeFunction;
+
+        public LambdaEqualityComparer(Func<T, T, bool> equalsFunction, Func<T, int> hashCodeFunction)
+        {
+            if (equalsFunction == null)
+                throw new ArgumentNullException(nameof(equalsFunction));
+
+            if (hashCodeFunction == null)
+                throw new ArgumentNullException(nameof(hashCodeFunction));
+
+            _equalsFunction = equalsFunction;
+            _hashCodeFunction = hashCodeFunction;
+        }
+
+        public bool Equals(T x, T y)
+        {
+            return _equalsFunction(x, y);
+        }
+
+        public int GetHashCode(T obj)
+        {
+            return _hashCodeFunction(obj);
+        }
+    }
+}
