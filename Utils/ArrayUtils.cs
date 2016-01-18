afc82f384b7480545e70f486310d6fdf50ebdc4f
Guillaume Teirlinck <guillaume.teirlinck@gmail.com>
2016-01-18 10:07:57 +0800

    Added utils class ArrayUtils

---
+++ Utils/ArrayUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Net.Teirlinck.Utils
+{
+    public static class ArrayUtils
+    {
+        /// <summary>
+        /// Returns a sub-array of an array
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sourceArray">The source array</param>
+        /// <param name="index">Start index (inclusive)</param>
+        /// <param name="length">Length of the sub-array</param>
+        /// <returns></returns>
+        public static T[] SubArray<T>(this T[] sourceArray, int index, int length)
+        {
+            if (sourceArray == null || sourceArray.Length == 0)
+                throw new ArgumentException(nameof(sourceArray));
+
+            if (index < 0 || index > sourceArray.Length - 1)
+                throw new ArgumentException(nameof(index));
+
+            if (length < 1 || index + length > sourceArray.Length - 1)
+                throw new ArgumentException(nameof(length));
+
+            T[] result = new T[length];
+            Array.Copy(sourceArray, index, result, 0, length);
+            return result;
+        }
+    }
+}
---
+++ UtilsTests/ArrayUtilsTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Net.Teirlinck.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilsTests
+{
+    [TestClass]
+    public class ArrayUtilsTests
+    {
+        [TestMethod]
+        public void TestSubArray()
+        {
+            int[] data = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            // Good case
+            try
+            {
+                int[] sub = data.SubArray(3, 4); // contains {3,4,5,6}
+
+                Assert.IsTrue(sub.Length == 4);
+                Assert.IsTrue(sub[0] == 3);
+                Assert.IsTrue(sub[3] == 6);
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+
+            // Bad case: sourceArray
+            try
+            {
+                int[] sub = (new int[0] { }).SubArray(-1, 4);
+
+                Assert.Fail();
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.IsTrue(ex.Message == "sourceArray");
+            }
+
+            // Bad case: index
+            try
+            {
+                int[] sub = data.SubArray(-1, 4);
+
+                Assert.Fail();
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.IsTrue(ex.Message == "index");
+            }
+
+            // Bad case: length
+            try
+            {
+                int[] sub = data.SubArray(3, 7);
+
+                Assert.Fail();
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.IsTrue(ex.Message == "length");
+            }
+        }
+    }
+}
