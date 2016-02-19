74d03779cd3fd5f619005a0523b86ad5547aa37b
Guillaume Teirlinck <guillaume.teirlinck@gmail.com>
2016-02-19 08:55:38 +0800

    Added CollectionExtensions methods

--- Utils/CollectionUtils.cs
+++ Utils/CollectionUtils.cs
@@ -22,4 +22,24 @@ namespace Net.Teirlinck.Utils
             return ((enumerable != null) && (enumerable.Count<T>() >= largerThan));
         }
     }
+
+    public static class CollectionExtensions
+    {
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerable)
+        {
+            return ((enumerable == null) || (enumerable.Count<T>() == 0));
+        }
+
+        /// <summary>
+        /// Returns true if an enumerable is not null and has a size larger than a certain minimum
+        /// </summary>
+        /// <typeparam name="T">The type of the generic enumerable</typeparam>
+        /// <param name="enumerable">The enumerable to test</param>
+        /// <param name="largerThan">The minimum sixze (inclusive)</param>
+        /// <returns></returns>
+        public static bool IsLargerThan<T>(this IEnumerable<T> enumerable, int largerThan)
+        {
+            return ((enumerable != null) && (enumerable.Count<T>() >= largerThan));
+        }
+    }
 }
