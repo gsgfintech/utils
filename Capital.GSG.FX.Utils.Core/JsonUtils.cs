a8f8ab3453c799ad642f5e910e7af4b45244ba0e
Guillaume Teirlinck <guillaume.teirlinck@gmail.com>
2017-04-30 07:30:36 +0800

    Add JsonUtils

---
+++ Capital.GSG.FX.Utils.Core/JsonUtils.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace Capital.GSG.FX.Utils.Core
+{
+    public static class JsonUtils
+    {
+        public static (bool IsValid, string ValidationMessage) IsValidJson(string value)
+        {
+            value = value.Trim();
+
+            if ((value.StartsWith("{") && value.EndsWith("}")) || //For object
+                (value.StartsWith("[") && value.EndsWith("]"))) //For array
+            {
+                try
+                {
+                    var obj = JToken.Parse(value);
+                    return (true, "");
+                }
+                catch (JsonReaderException ex)
+                {
+                    return (false, ex.Message);
+                }
+                catch (Exception ex) //some other exception
+                {
+                    return (false, ex.Message);
+                }
+            }
+            else
+                return (false, "The Json string should start with '{' or '['");
+        }
+
+        public static bool TryParseJson(string value, out JToken jToken)
+        {
+            value = value.Trim();
+
+            if ((value.StartsWith("{") && value.EndsWith("}")) || //For object
+                (value.StartsWith("[") && value.EndsWith("]"))) //For array
+            {
+                try
+                {
+                    jToken = JToken.Parse(value);
+                    return true;
+                }
+                catch (Exception) { }
+            }
+
+            jToken = null;
+            return false;
+        }
+    }
+}
---
+++ Capital.GSG.FX.Utils.Tests/JsonUtilsTests.cs
@@ -0,0 +1,56 @@
+﻿using Capital.GSG.FX.Utils.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capital.GSG.FX.Utils.Tests
+{
+    [TestClass]
+    public class JsonUtilsTests
+    {
+        [TestMethod]
+        public void TestIsValidJson()
+        {
+            string valid = @"{ 'Name': 'SomeName' }";
+
+            var result = JsonUtils.IsValidJson(valid);
+            Assert.IsTrue(result.IsValid, result.ValidationMessage);
+
+            string invalid = "BadJson";
+
+            result = JsonUtils.IsValidJson(invalid);
+            Assert.IsFalse(result.IsValid);
+
+            invalid = "{BadJson}";
+
+            result = JsonUtils.IsValidJson(invalid);
+            Assert.IsFalse(result.IsValid);
+        }
+
+        [TestMethod]
+        public void TestTryParseJson()
+        {
+            JToken jtoken;
+
+            string valid = @"{ 'Name': 'SomeName' }";
+
+            var result = JsonUtils.TryParseJson(valid, out jtoken);
+            Assert.IsTrue(result);
+            Assert.IsNotNull(jtoken);
+
+            string invalid = "BadJson";
+
+            result = JsonUtils.TryParseJson(invalid, out jtoken);
+            Assert.IsFalse(result);
+            Assert.IsNull(jtoken);
+
+            invalid = "{BadJson}";
+
+            result = JsonUtils.TryParseJson(invalid, out jtoken);
+            Assert.IsFalse(result);
+            Assert.IsNull(jtoken);
+        }
+    }
+}
